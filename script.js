851283d9f62af26f471687f68f9145b0c2b0bad9
amabilybedutti-ai <amabily.bedutti@escola.pr.gov.br>
2026-06-09 11:51:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+//0 JavaScript é uma lingagem WEB
+
+//variáveis-valores armazenados na memória do computador 
+
+//Funções- Blocos de código
+
+function carregar (){
+    //alert ("Bem vindo ao java script")
+    var msg = window.document.getElementById
+    ('msg')
+    msg.innerHTML = `desenvolvendo com DOM`
+}
