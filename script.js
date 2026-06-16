01c1c4f4323711175690495ef220546d6af22625
amabilybedutti-ai <amabily.bedutti@escola.pr.gov.br>
2026-06-16 12:03:03 -0300

    alteracao horarios

--- script.js
+++ script.js
@@ -8,5 +8,24 @@ function carregar (){
     //alert ("Bem vindo ao java script")
     var msg = window.document.getElementById
     ('msg')
-    msg.innerHTML = `desenvolvendo com DOM`
+    var img = window.document.getElementById('imagem')
+
+    var data = new Date ()
+    var hora = data.getHours()
+    var minutos = data.getMinutes()
+
+    msg.innerHTML = `agora sao ${hora}:${minutos}am`
+
+    if(hora>=0&& hora< 12){
+        img.src = 'manha.png'
+        document.body.style.background ='#caf0f8'  
+    }
+    else if (hora>= 12 && hora < 18){
+        img.src = 'tarde.png'
+        document.body.style.background ='#f7d6e0'  
+    }
+    else{
+        img.src = 'noite.png'
+        document.body.style.background ='#d3c3eb' 
+    }
 }
